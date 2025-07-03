23c8b8eda579f987b37a8709a06baadab20b5dd1
Iannn-vbeta <232410103085@mail.unej.ac.id>
2025-07-03 22:09:38 +0700

    Layoting

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from "react-router-dom";
+
+function RootLayout() {
+  return (
+    <div className="container mx-auto p-4">
+      <header className="mb-8">
+        <nav className="flex justify-between items-center bg-gray-800 p-4 rounded-lg shadow-lg">
+          <Link to="/" className="text-white text-lg font-semibold">
+            Home
+          </Link>
+          <Link to="/about" className="text-white text-lg font-semibold">
+            About
+          </Link>
+        </nav>
+      </header>
+      <main>
+        {/* mirip @yield laravel */}
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default RootLayout;
